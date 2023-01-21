aa89fc295203637f2a968abeb6a7b8ad89df2f19
Joseph Graffeo <josephvgraffeo1@gmail.com>
2023-01-21 13:51:53 -0500

    more work on the problems

--- beginnerQuestionWork.js
+++ beginnerQuestionWork.js
@@ -9,8 +9,10 @@ const getEven = (array) => {
     console.log(evenArray);
 };
 
+console.log("\n");
 getEven([0,1,2,3,4,5,6,7,8,9,10]);
 console.log("\n");
+
 
 // 2. print a multiplication table
 const multTable = (x,y) => {
@@ -25,6 +27,7 @@ const multTable = (x,y) => {
 };
 multTable(5,5);
 
+
 // 3. kilometers to miles converter function
 const kilometersToMiles = (kilo) => {
     let kiloToMile = kilo * 0.62137119
@@ -33,16 +36,50 @@ const kilometersToMiles = (kilo) => {
 kilometersToMiles(15);
 console.log("\n");
 
-// 4. calculate the sum of numbers in an array
-const arraySum = () => {
 
+// 4. calculate the sum of numbers in an array
+const arraySum = (array) => {
+    let sum = 0
+    for (i = 0; i <+ array.length; i++) {
+        sum += array[i];
+   }
+   console.log(sum);
 };
 
+arraySum([10,20,20,100]);
+
+
 // 5. create a function that reverses an array
+const reverseArray = (array) => {
+    console.log(array.reverse());
+}
+
+console.log(`\n`);
+reverseArray([1,2,3,4,5]);
+
 
 // 6. sort an array from lowest to highest
+const sortArray = (array) => {
+    let sortedArray = array.sort((a, b) => a - b);
+    console.log(sortedArray);
+};
+
+console.log(`\n`);
+sortArray([100,86,1,134,12,12,96]);
+
 
 // 7. function that filters out negative numbers
+let positiveArray = [];
+const noNegatives = (array) => {
+    for (i = 0; i <= array.length; i++) {
+        if (array[i] >= 0) {
+            positiveArray.push(array[i]);
+        };
+    }; console.log(positiveArray);
+};
+
+console.log(`\n`);
+noNegatives([-1,-2,-4,-100, 0, 100, 200]);
 
 // 8. remove spaces in a string
 
